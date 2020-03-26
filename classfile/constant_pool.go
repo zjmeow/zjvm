d9c03c552c5d5e0ea6fa61aac5debc57debd7f45
zjmeow <zjmeow@gmail.com>
2020-03-26 22:59:28 +0800

    member info

--- classfile/constant_pool.go
+++ classfile/constant_pool.go
@@ -3,3 +3,14 @@ package classfile
 type ConstantPool []ConstantInfo
 
 func readConstantPool(reader *ClassReader) {}
+
+func (cp *ConstantPool) getConstantInfo(index uint16) ConstantInfo {
+	if cpInfo := (*cp)[index]; cpInfo != nil {
+		return cpInfo
+	}
+	panic("Invalid constant pool index")
+}
+
+func (cp *ConstantPool) getUtf8(index uint16) string {
+	return ""
+}
--- classfile/member_info.go
+++ classfile/member_info.go
@@ -11,3 +11,29 @@ type MemberInfo struct {
 type ConstantInfo interface {
 	readInfo(reader *ClassReader)
 }
+
+func readMembers(reader *ClassReader, cp ConstantPool) []*MemberInfo {
+	count := reader.readUint16()
+	members := make([]*MemberInfo, count)
+	for i := range members {
+		members[i] = readMember(reader, cp)
+	}
+	return members
+}
+
+func readMember(reader *ClassReader, cp ConstantPool) *MemberInfo {
+	return &MemberInfo{
+		cp:              cp,
+		accessFlags:     reader.readUint16(),
+		nameIndex:       reader.readUint16(),
+		descriptorIndex: reader.readUint16(),
+		attributes:      nil,
+	}
+}
+func (mi *MemberInfo) Name() string {
+	return mi.cp.getUtf8(mi.nameIndex)
+}
+
+func (mi *MemberInfo) Descriptor() string {
+	return mi.cp.getUtf8(mi.descriptorIndex)
+}
