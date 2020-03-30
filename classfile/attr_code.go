20fa64bb335e8af2986609949e15e527a7caec16
zjmeow <zjmeow@gmail.com>
2020-03-30 23:18:40 +0800

    code attribute

---
+++ classfile/attr_code.go
@@ -0,0 +1,39 @@
+package classfile
+
+type CodeAttribute struct {
+	cp             ConstantPool
+	maxStack       uint16
+	maxLocals      uint16
+	code           []byte
+	exceptionTable []*ExceptionTableEntry
+	attributes     []AttributeInfo
+}
+type ExceptionTableEntry struct {
+	startPc   uint16
+	endPc     uint16
+	handlerPc uint16
+	catchType uint16
+}
+
+func (ca *CodeAttribute) readInfo(reader *ClassReader) {
+	ca.maxStack = reader.readUint16()
+	ca.maxLocals = reader.readUint16()
+	codeLength := reader.readUint32()
+	ca.code = reader.readBytes(codeLength)
+	ca.exceptionTable = readExceptionTable(reader)
+	ca.attributes = readAttributes(reader, ca.cp)
+}
+
+func readExceptionTable(reader *ClassReader) []*ExceptionTableEntry {
+	count := reader.readUint16()
+	exceptions := make([]*ExceptionTableEntry, count)
+	for i := range exceptions {
+		exceptions[i] = &ExceptionTableEntry{
+			startPc:   reader.readUint16(),
+			endPc:     reader.readUint16(),
+			handlerPc: reader.readUint16(),
+			catchType: reader.readUint16(),
+		}
+	}
+	return exceptions
+}
