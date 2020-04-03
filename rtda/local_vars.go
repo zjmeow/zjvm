ef0db5b9b7632ad311b6aeb86b3527402b06bb8c
zjmeow <zjmeow@gmail.com>
2020-04-03 23:12:17 +0800

    local variable

--- rtda/local_vars.go
+++ rtda/local_vars.go
@@ -1,3 +1,50 @@
 package rtda
 
+import "math"
+
 type LocalVars []Slot
+
+func newLocalVars(size uint) LocalVars {
+	if size > 0 {
+		return make([]Slot, size)
+	}
+	return nil
+}
+
+func (lv LocalVars) SetInt(index uint, val int32) {
+	lv[index].num = val
+}
+func (lv LocalVars) GetInt(index uint) int32 {
+	return lv[index].num
+}
+func (lv LocalVars) SetFloat(index uint, val float32) {
+	lv[index].num = int32(math.Float32bits(val))
+}
+func (lv LocalVars) GetFloat(index uint) float32 {
+	bits := uint32(lv[index].num)
+	return math.Float32frombits(bits)
+}
+func (lv LocalVars) SetLong(index uint, val int64) {
+	lv[index].num = int32(val)
+	lv[index+1].num = int32(val >> 32)
+}
+func (lv LocalVars) GetLong(index uint) int64 {
+	low := lv[index].num
+	high := lv[index+1].num
+	return int64(high)<<32 | int64(low)
+}
+func (lv LocalVars) SetDouble(index uint, val float64) {
+	bits := int64(math.Float64bits(val))
+	lv.SetLong(index, bits)
+}
+func (lv LocalVars) GetDouble(index uint) float64 {
+	bits := uint64(lv.GetLong(index))
+	return math.Float64frombits(bits)
+}
+
+func (lv LocalVars) SetRef(index uint, val *Object) {
+	lv[index].ref = val
+}
+func (lv LocalVars) GetRef(index uint) *Object {
+	return lv[index].ref
+}
