044e5b86360470e26b50d91917802fdba8bdecb9
zjmeow <zjmeow@gmail.com>
2020-04-03 22:40:57 +0800

    jvm stack

--- rtda/jvm_stack.go
+++ rtda/jvm_stack.go
@@ -3,5 +3,34 @@ package rtda
 type Stack struct {
 	maxSize uint
 	size    uint
-	top     *Frame
+	_top    *Frame
+}
+
+func newStack(maxSize uint) *Stack {
+	return &Stack{maxSize: maxSize}
+}
+
+func (s Stack) push() {
+	if s.size > s.maxSize {
+		panic("java.lang.StackOverflowError")
+	}
+	s.size += 1
+}
+
+func (s Stack) pop() *Frame {
+	if s._top == nil {
+		panic("jvm stack is empty")
+	}
+	top := s._top
+	s._top = top.lower
+	top.lower = nil
+	s.size--
+	return top
+}
+
+func (s Stack) top() *Frame {
+	if s._top == nil {
+		panic("jvm stack is empty")
+	}
+	return s._top
 }
