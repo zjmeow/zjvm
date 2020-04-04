7084c22ea86648afd52afce1a543f5be0bb509dc
zjmeow <zjmeow@gmail.com>
2020-04-04 22:06:47 +0800

    bytecode reader

---
+++ instructions/base/bytecode_reader.go
@@ -0,0 +1,37 @@
+package base
+
+type BytecodeReader struct {
+	code []byte
+	pc   int
+}
+
+func (br *BytecodeReader) Reset(code []byte, pc int) {
+	br.code = code
+	br.pc = pc
+}
+
+func (br *BytecodeReader) ReadUint8() uint8 {
+	res := br.code[br.pc]
+	br.pc++
+	return res
+}
+
+func (br *BytecodeReader) ReadInt8() int8 {
+	return int8(br.ReadUint8())
+}
+
+func (br *BytecodeReader) ReadUint16() uint16 {
+	high := uint16(br.ReadUint8())
+	low := uint16(br.ReadUint8())
+	return (high << 8) | low
+}
+
+func (br *BytecodeReader) ReadInt16() int16 {
+	return int16(br.ReadUint16())
+}
+
+func (br *BytecodeReader) ReadInt32() int32 {
+	high := int32(br.ReadUint16())
+	low := int32(br.ReadUint16())
+	return (high << 16) | low
+}
