4ea3b5fe33b0d459454acc2304e092619c6115c8
zjmeow <zjmeow@gmail.com>
2020-04-04 22:30:22 +0800

    instruction interface

---
+++ instructions/base/instruction.go
@@ -0,0 +1,48 @@
+package base
+
+import "../../rtda"
+
+type Instruction interface {
+	Execute(frame *rtda.Frame)
+	FetchOperands(reader *BytecodeReader)
+}
+
+type NoOperandsInstruction struct {
+}
+type BranchInstruction struct {
+	offset int
+}
+
+type Index8Instruction struct {
+	index uint
+}
+type Index16Instruction struct {
+	index uint
+}
+
+func (np *NoOperandsInstruction) Execute(frame *rtda.Frame) {
+
+}
+func (np *NoOperandsInstruction) FetchOperands(reader *BytecodeReader) {
+
+}
+
+func (bi *BranchInstruction) Execute(frame *rtda.Frame) {
+
+}
+func (bi *BranchInstruction) FetchOperands(reader *BytecodeReader) {
+	bi.offset = int(reader.ReadInt16())
+}
+
+func (i *Index8Instruction) Execute(frame *rtda.Frame) {
+
+}
+func (i *Index8Instruction) FetchOperands(reader *BytecodeReader) {
+	i.index = uint(reader.ReadUint8())
+}
+func (i *Index16Instruction) Execute(frame *rtda.Frame) {
+
+}
+func (i *Index16Instruction) FetchOperands(reader *BytecodeReader) {
+	i.index = uint(reader.ReadUint16())
+}
