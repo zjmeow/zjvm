f9ef8d375c1421b407a5a30e6a5ac9af64b63694
zjmeow <zjmeow@gmail.com>
2020-06-02 18:35:50 +0800

    refactor const instructions

--- instructions/constants/const.go
+++ instructions/constants/const.go
@@ -6,70 +6,53 @@ import (
 )
 
 type ACONST_NULL struct{ base.NoOperandsInstruction }
-type DCONST_0 struct{ base.NoOperandsInstruction }
-type DCONST_1 struct{ base.NoOperandsInstruction }
-type FCONST_0 struct{ base.NoOperandsInstruction }
-type FCONST_1 struct{ base.NoOperandsInstruction }
-type FCONST_2 struct{ base.NoOperandsInstruction }
-type ICONST_M1 struct{ base.NoOperandsInstruction }
-type ICONST_0 struct{ base.NoOperandsInstruction }
-type ICONST_1 struct{ base.NoOperandsInstruction }
-type ICONST_2 struct{ base.NoOperandsInstruction }
-type ICONST_3 struct{ base.NoOperandsInstruction }
-type ICONST_4 struct{ base.NoOperandsInstruction }
-type ICONST_5 struct{ base.NoOperandsInstruction }
-type LCONST_0 struct{ base.NoOperandsInstruction }
-type LCONST_1 struct{ base.NoOperandsInstruction }
+
+type IntConst struct {
+	base.NoOperandsInstruction
+	num int32
+}
+type LongConst struct {
+	base.NoOperandsInstruction
+	num int64
+}
+
+type FloatConst struct {
+	base.NoOperandsInstruction
+	num float32
+}
+
+type DoubleConst struct {
+	base.NoOperandsInstruction
+	num float64
+}
+
+func NewConstInt(num int32) base.Instruction {
+	return &IntConst{num: num}
+}
+func NewConstLong(num int64) base.Instruction {
+	return &LongConst{num: num}
+}
+func NewConstFloat(num float32) base.Instruction {
+	return &FloatConst{num: num}
+}
+
+func NewConstDouble(num float64) base.Instruction {
+	return &DoubleConst{num: num}
+}
 
 func (ins *ACONST_NULL) Execute(frame *rtda.Frame) {
 	frame.OperandStack().PushRef(nil)
 }
 
-func (ins *DCONST_0) Execute(frame *rtda.Frame) {
-	frame.OperandStack().PushDouble(0)
+func (ins *IntConst) Execute(frame *rtda.Frame) {
+	frame.OperandStack().PushInt(ins.num)
 }
-
-func (ins *DCONST_1) Execute(frame *rtda.Frame) {
-	frame.OperandStack().PushDouble(1)
+func (ins *LongConst) Execute(frame *rtda.Frame) {
+	frame.OperandStack().PushLong(ins.num)
 }
-
-func (ins *FCONST_0) Execute(frame *rtda.Frame) {
-	frame.OperandStack().PushFloat(0)
+func (ins *FloatConst) Execute(frame *rtda.Frame) {
+	frame.OperandStack().PushFloat(ins.num)
 }
-
-func (ins *FCONST_1) Execute(frame *rtda.Frame) {
-	frame.OperandStack().PushFloat(1)
-}
-
-func (ins *FCONST_2) Execute(frame *rtda.Frame) {
-	frame.OperandStack().PushFloat(2)
-}
-
-func (ins *ICONST_M1) Execute(frame *rtda.Frame) {
-	frame.OperandStack().PushInt(-1)
-}
-func (ins *ICONST_0) Execute(frame *rtda.Frame) {
-	frame.OperandStack().PushInt(0)
-}
-func (ins *ICONST_1) Execute(frame *rtda.Frame) {
-	frame.OperandStack().PushInt(1)
-}
-
-func (ins *ICONST_2) Execute(frame *rtda.Frame) {
-	frame.OperandStack().PushInt(2)
-}
-func (ins *ICONST_3) Execute(frame *rtda.Frame) {
-	frame.OperandStack().PushInt(3)
-}
-func (ins *ICONST_4) Execute(frame *rtda.Frame) {
-	frame.OperandStack().PushInt(4)
-}
-func (ins *ICONST_5) Execute(frame *rtda.Frame) {
-	frame.OperandStack().PushInt(5)
-}
-func (ins *LCONST_0) Execute(frame *rtda.Frame) {
-	frame.OperandStack().PushLong(0)
-}
-func (ins *LCONST_1) Execute(frame *rtda.Frame) {
-	frame.OperandStack().PushLong(1)
+func (ins *DoubleConst) Execute(frame *rtda.Frame) {
+	frame.OperandStack().PushDouble(ins.num)
 }
