3136e82d15ceb9b6fcf826626eddbbe40fc44773
zjmeow <zjmeow@gmail.com>
2020-08-30 20:59:29 +0800

    方法查找

--- rtda/heap/cp_method_ref.go
+++ rtda/heap/cp_method_ref.go
@@ -13,3 +13,38 @@ func newMethodRef(cp *ConstantPool, refInfo *classfile.ConstantMethodRefInfo) *M
 	ref.copyMemberRefInfo(&refInfo.ConstantMemberRefInfo)
 	return ref
 }
+
+func (m *MethodRef) ResolvedMethod() *Method {
+	if m.method == nil {
+		m.resolveMethodRef()
+	}
+	return m.method
+}
+
+//
+func (m *MethodRef) resolveMethodRef() {
+	// 被引用的类
+	class := m.ResolveClass()
+	// 接口的方法不能调用
+	if class.IsInterface() {
+		panic("java.lang.IncompatibleClassChangeError")
+	}
+	method := lookupMethod(class, m.name, m.descriptor)
+	// 找不到方法
+	if method == nil {
+		panic("java.lang.NoSuchMethodError")
+	}
+	// 没有访问权限
+	if !method.isAccessibleTo(m.cp.class) {
+		panic("java.lang.IllegalAccessError")
+	}
+	m.method = method
+}
+
+func lookupMethod(class *Class, name, descriptor string) *Method {
+	method := LookupMethodInClass(class, name, descriptor)
+	if method == nil {
+		method = LookupMethodInterfaces(class.interfaces, name, descriptor)
+	}
+	return method
+}
---
+++ rtda/heap/method_lookup.go
@@ -0,0 +1,28 @@
+package heap
+
+func LookupMethodInClass(class *Class, name, descriptor string) *Method {
+	for c := class; c != nil; c = c.superClass {
+		for _, method := range c.methods {
+			if method.name == name && method.descriptor == descriptor {
+				return method
+			}
+		}
+	}
+	return nil
+}
+
+func LookupMethodInterfaces(ifaces []*Class, name, descriptor string) *Method {
+	for _, iface := range ifaces {
+		for _, method := range iface.methods {
+			if method.name == name && method.descriptor == descriptor {
+				return method
+			}
+		}
+		// 接口可以多重继承
+		method := LookupMethodInterfaces(iface.interfaces, name, descriptor)
+		if method != nil {
+			return method
+		}
+	}
+	return nil
+}
